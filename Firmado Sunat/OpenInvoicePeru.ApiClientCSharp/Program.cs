d2ce600ea4774ae23cd650d2f06c67dbe80a4737
Erick Orlando <erickorlando@outlook.com>
2016-07-29 22:58:49 -0500

    Construccion de cliente en CSharp con .NET 4.0

--- Firmado Sunat/OpenInvoicePeru.ApiClientCSharp/Program.cs
+++ Firmado Sunat/OpenInvoicePeru.ApiClientCSharp/Program.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RestSharp;
 
 namespace OpenInvoicePeru.ApiClientCSharp
 {
     class Program
     {
+        private static string _baseUrl = "http://localhost:50888/OpenInvoicePeru/api";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Prueba de API REST de OpenInvoicePeru");
+            var client = new RestClient(_baseUrl);
 
+            var request = new RestRequest("invoice", Method.POST);
+            
         }
     }
 }
