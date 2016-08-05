d450ce07a0f874aacae23b632cf9c8c639c1138b
Erick Orlando <erickorlando@outlook.com>
2016-08-05 13:46:41 -0500

    Consulta de Ticket a través de API REST

--- Firmado Sunat/ErickOrlando.FirmadoSunatWin/frmEnviarSunat.cs
+++ Firmado Sunat/ErickOrlando.FirmadoSunatWin/frmEnviarSunat.cs
@@ -20,7 +20,7 @@ namespace OpenInvoicePeru.FirmadoSunatWin
     public partial class FrmEnviarSunat : Form
     {
         private FrmDocumento _frmDocumento;
-        private HttpClient _client;
+        private readonly HttpClient _client;
 
         public FrmEnviarSunat()
         {
@@ -86,6 +86,7 @@ namespace OpenInvoicePeru.FirmadoSunatWin
         {
             try
             {
+                Cursor = Cursors.WaitCursor;
                 string codigoTipoDoc;
                 switch (cboTipoDoc.SelectedIndex)
                 {
@@ -117,132 +118,6 @@ namespace OpenInvoicePeru.FirmadoSunatWin
                         codigoTipoDoc = "01";
                         break;
                 }
-                //// Una vez validado el XML seleccionado procedemos con obtener el Certificado.
-                //var serializar = new Serializador
-                //{
-                //    RutaCertificadoDigital = Convert.ToBase64String(File.ReadAllBytes(txtRutaCertificado.Text)),
-                //    PasswordCertificado = txtPassCertificado.Text,
-                //    TipoDocumento = rbRetenciones.Checked ? 0 : 1
-                //};
-
-                //// Validacion extra cuando sea un documento de resumen.
-                //if (rbResumen.Checked) serializar.TipoDocumento = 0;
-
-                //var param = new ConexionSunat.Parametros
-                //{
-                //    EndPointUrl = ValorSeleccionado(),
-                //    Retencion = rbRetenciones.Checked,
-                //    Ruc = txtNroRuc.Text,
-                //    UserName = txtUsuarioSol.Text,
-                //    Password = txtClaveSol.Text
-                //};
-                //using (var conexion = new ConexionSunat(param))
-                //{
-                //    var byteArray = File.ReadAllBytes(txtSource.Text);
-
-                //    Cursor = Cursors.WaitCursor;
-
-                //    // Firmamos el XML.
-                //    var tramaFirmado = serializar.FirmarXml(Convert.ToBase64String(byteArray));
-                //    // Le damos un nuevo nombre al archivo
-                //    var nombreArchivo = $"{txtNroRuc.Text}-{codigoTipoDoc}-{txtSerieCorrelativo.Text}";
-                //    // Escribimos el archivo XML ya firmado en una nueva ubicación.
-                //    using (var fs = File.Create($"{nombreArchivo}.xml"))
-                //    {
-                //        var byteFirmado = Convert.FromBase64String(tramaFirmado);
-                //        fs.Write(byteFirmado, 0, byteFirmado.Length);
-                //    }
-
-                //    // Ahora lo empaquetamos en un ZIP.
-                //    var tramaZip = serializar.GenerarZip(tramaFirmado, nombreArchivo);
-
-                //    if (rbResumen.Checked)
-                //    {
-                //        var rptaSunat = conexion.EnviarResumenBaja(tramaZip, $"{nombreArchivo}.zip");
-
-                //        if (rptaSunat.Item2)
-                //        {
-                //            var sb = new StringBuilder();
-
-                //            // Añadimos la respuesta del Servicio.
-                //            sb.AppendLine(Resources.procesoCorrecto);
-
-                //            sb.AppendLine($"Procesamiento correcto, el numero de Ticket es {rptaSunat.Item1}");
-
-
-                //            txtResult.Text = sb.ToString();
-                //            sb.Length = 0;
-                //        }
-                //        else
-                //            txtResult.Text = rptaSunat.Item1;
-                //    }
-                //    else
-                //    {
-                //        var resultado = conexion.EnviarDocumento(tramaZip, $"{nombreArchivo}.zip");
-
-                //        if (resultado.Item2)
-                //        {
-                //            var returnByte = Convert.FromBase64String(resultado.Item1);
-
-                //            var rutaArchivo = $"{Directory.GetCurrentDirectory()}\\R-{nombreArchivo}.zip";
-                //            var fs = new FileStream(rutaArchivo, FileMode.Create, FileAccess.Write);
-                //            fs.Write(returnByte, 0, returnByte.Length);
-                //            fs.Close();
-
-                //            var sb = new StringBuilder();
-
-                //            // Añadimos la respuesta del Servicio.
-                //            sb.AppendLine(Resources.procesoCorrecto);
-
-                //            // Procedemos a desempaquetar el archivo y leer el contenido de la respuesta SUNAT.
-                //            using (var streamZip = ZipFile.Read(File.Open(rutaArchivo,
-                //                FileMode.Open,
-                //                FileAccess.ReadWrite)))
-                //            {
-                //                foreach (var entry in streamZip.Entries)
-                //                {
-                //                    if (!entry.FileName.EndsWith(".xml")) continue;
-                //                    using (var ms = new MemoryStream())
-                //                    {
-                //                        entry.Extract(ms);
-                //                        ms.Position = 0;
-                //                        var responseReader = new StreamReader(ms);
-                //                        var responseString = responseReader.ReadToEnd();
-                //                        try
-                //                        {
-                //                            var xmlDoc = new XmlDocument();
-                //                            xmlDoc.LoadXml(responseString);
-
-                //                            var xmlnsManager = new XmlNamespaceManager(xmlDoc.NameTable);
-
-                //                            xmlnsManager.AddNamespace("ar", EspacioNombres.ar);
-                //                            xmlnsManager.AddNamespace("cac",EspacioNombres.cac);
-                //                            xmlnsManager.AddNamespace("cbc",EspacioNombres.cbc);
-
-                //                            var responseCode = xmlDoc.SelectSingleNode(EspacioNombres.nodoResponseCode,
-                //                                xmlnsManager)?.InnerText;
-                //                            var description = xmlDoc.SelectSingleNode(EspacioNombres.nodoDescription,
-                //                                xmlnsManager)?.InnerText;
-
-                //                            sb.AppendFormat("Código:{0}\n Descripción:{1}", responseCode, description);
-
-                //                        }
-                //                        catch (Exception ex)
-                //                        {
-                //                            sb.AppendLine(ex.Message);
-                //                        }
-                //                    }
-                //                }
-                //            }
-
-                //            txtResult.Text = sb.ToString();
-                //            sb.Length = 0; // Limpiamos memoria del StringBuilder.
-                //        }
-                //        else
-                //            txtResult.Text = resultado.Item1;
-                //    }
-
-                //}
 
                 var tramaXmlSinFirma = Convert.ToBase64String(File.ReadAllBytes(txtSource.Text));
 
@@ -251,7 +126,7 @@ namespace OpenInvoicePeru.FirmadoSunatWin
                     TramaXmlSinFirma = tramaXmlSinFirma,
                     CertificadoDigital = Convert.ToBase64String(File.ReadAllBytes(txtRutaCertificado.Text)),
                     PasswordCertificado = txtPassCertificado.Text,
-                    UnSoloNodoExtension = !rbRetenciones.Checked
+                    UnSoloNodoExtension = rbRetenciones.Checked
                 };
 
                 var jsonFirmado = await _client.PostAsJsonAsync("api/Firmar", firmadoRequest);
@@ -276,7 +151,7 @@ namespace OpenInvoicePeru.FirmadoSunatWin
 
                 if (!respuestaEnvio.Exito)
                     throw new ApplicationException(respuestaEnvio.MensajeError);
-
+ 
                 txtResult.Text = $"{Resources.procesoCorrecto}{Environment.NewLine}{respuestaEnvio.MensajeRespuesta}";
 
                 if (chkVoz.Checked)
@@ -313,84 +188,36 @@ namespace OpenInvoicePeru.FirmadoSunatWin
             }
         }
 
-        private void btnGetStatus_Click(object sender, EventArgs e)
+        private async void btnGetStatus_Click(object sender, EventArgs e)
         {
             try
             {
+                Cursor = Cursors.WaitCursor;
+
                 using (var frm = new FrmTicket())
                 {
                     if (frm.ShowDialog() != DialogResult.OK) return;
                     if (string.IsNullOrEmpty(frm.txtNroTicket.Text)) return;
 
-                    var param = new ConexionSunat.Parametros
+
+                    var consultaTicketRequest = new ConsultaTicketRequest
                     {
-                        EndPointUrl = ValorSeleccionado(),
-                        Retencion = rbRetenciones.Checked,
                         Ruc = txtNroRuc.Text,
-                        UserName = txtUsuarioSol.Text,
-                        Password = txtClaveSol.Text
+                        UsuarioSol = txtUsuarioSol.Text,
+                        ClaveSol = txtClaveSol.Text,
+                        EndPointUrl = ValorSeleccionado(),
+                        IdDocumento = txtSerieCorrelativo.Text,
+                        NroTicket = frm.txtNroTicket.Text
                     };
-                    using (var conexion = new ConexionSunat(param))
-                    {
-                        var resultado = conexion.ObtenerEstado(frm.txtNroTicket.Text);
 
-                        if (resultado.Item2)
-                        {
-                            var returnByte = Convert.FromBase64String(resultado.Item1);
+                    var jsonConsultaTicket = await _client.PostAsJsonAsync("api/ConsultarTicket", consultaTicketRequest);
 
-                            var rutaArchivo = $"{Directory.GetCurrentDirectory()}\\R-{frm.txtNroTicket.Text}.zip";
-                            var fs = new FileStream(rutaArchivo, FileMode.Create, FileAccess.Write);
-                            fs.Write(returnByte, 0, returnByte.Length);
-                            fs.Close();
+                    var respuestaEnvio = await jsonConsultaTicket.Content.ReadAsAsync<EnviarDocumentoResponse>();
 
-                            var sb = new StringBuilder();
+                    if (!respuestaEnvio.Exito)
+                        throw new ApplicationException(respuestaEnvio.MensajeError);
 
-                            // Añadimos la respuesta del Servicio.
-                            sb.AppendLine(Resources.procesoCorrecto);
-
-                            // Extraemos el XML contenido en el archivo de respuesta como un XML.
-                            var rutaArchivoXmlRespuesta = rutaArchivo.Replace(".zip", ".xml");
-                            // Procedemos a desempaquetar el archivo y leer el contenido de la respuesta SUNAT.
-                            using (var streamZip = ZipFile.Read(File.Open(rutaArchivo,
-                                FileMode.Open,
-                                FileAccess.ReadWrite, FileShare.ReadWrite)))
-                            {
-                                // Nos aseguramos de que el ZIP contiene al menos un elemento.
-                                if (streamZip.Entries.Any())
-                                {
-                                    if (rbRetenciones.Checked)
-                                        streamZip.Entries.Last()
-                                        .Extract(".", ExtractExistingFileAction.OverwriteSilently);
-                                    else
-                                        streamZip.Entries.First()
-                                            .Extract(".", ExtractExistingFileAction.OverwriteSilently);
-                                }
-
-                            }
-                            // Como ya lo tenemos extraido, leemos el contenido de dicho archivo.
-                            var xDoc = XDocument.Parse(File.ReadAllText(rutaArchivoXmlRespuesta));
-
-                            var respuesta = xDoc.Descendants(XName.Get("DocumentResponse", EspacioNombres.cac))
-                                .Descendants(XName.Get("Response", EspacioNombres.cac))
-                                .Descendants().ToList();
-
-                            if (respuesta.Any())
-                            {
-                                // La respuesta se compone de 3 valores
-                                // cbc:ReferenceID
-                                // cbc:ResponseCode
-                                // cbc:Description
-                                // Obtendremos unicamente la Descripción (la última).
-                                sb.AppendLine($"Respuesta de SUNAT a las {DateTime.Now}");
-                                sb.AppendLine(((XText)respuesta.Nodes().Last()).Value);
-                            }
-
-                            txtResult.Text = sb.ToString();
-                            sb.Length = 0; // Limpiamos memoria del StringBuilder.  
-                        }
-                        else
-                            txtResult.Text = resultado.Item1;
-                    }
+                    txtResult.Text = $"{Resources.procesoCorrecto}{Environment.NewLine}{respuestaEnvio.MensajeRespuesta}";
 
                     Hablar();
                 }
@@ -398,6 +225,10 @@ namespace OpenInvoicePeru.FirmadoSunatWin
             catch (Exception ex)
             {
                 txtResult.Text = ex.Message;
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
             }
         }
 
