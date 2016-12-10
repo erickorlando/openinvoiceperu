e696329d6847a18c0c484ca79946ad6580931384
Erick Orlando <erickorlando@outlook.com>
2016-12-09 19:08:25 -0500

    #33 Nueva API REST con Generación de Documentos de Retencion

---
+++ OpenInvoicePeru/OpenInvoicePeru.WebApi/Controllers/GenerarRetencionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http;
+using OpenInvoicePeru.Firmado.Models;
+using OpenInvoicePeru.Firmado;
+
+namespace OpenInvoicePeru.WebApi.Controllers
+{
+    public class GenerarRetencionController : ApiController
+    {
+        public DocumentoResponse Post([FromBody] DocumentoRetencion documento)
+        {
+            var response = new DocumentoResponse();
+            try
+            {
+                var invoice = Generador.GenerarRetention(documento);
+
+                var serializador = new Serializador();
+
+                response.TramaXmlSinFirma = serializador.GenerarXml(invoice);
+                response.Exito = true;
+            }
+            catch (Exception ex)
+            {
+                response.MensajeError = ex.Message;
+                response.Pila = ex.StackTrace;
+                response.Exito = false;
+            }
+
+            return response;
+        }
+    }
+}
