d2ca94edc6a15888d3726749243e8173bb692554
Erick Orlando <erickorlando@outlook.com>
2016-12-12 17:24:17 -0500

    #20 Pruebas Unitarias para Serializador

---
+++ OpenInvoicePeru/OpenInvoicePeru.Tests/Fake/SerializadorFake.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using OpenInvoicePeru.Comun;
+using OpenInvoicePeru.Comun.Dto.Intercambio;
+using OpenInvoicePeru.Firmado;
+
+namespace OpenInvoicePeru.Tests.Fake
+{
+    public class SerializadorFake : ISerializador
+    {
+        public async Task<string> GenerarXml<T>(T objectToSerialize) where T : IEstructuraXml
+        {
+            return await Task.Factory.StartNew(objectToSerialize.ToString);
+        }
+
+        async Task<string> ISerializador.GenerarZip(string tramaXml, string nombreArchivo)
+        {
+            return await Task.Factory.StartNew(() => string.Concat(tramaXml, nombreArchivo));
+        }
+
+        async Task<EnviarDocumentoResponse> ISerializador.GenerarDocumentoRespuesta(string constanciaRecepcion)
+        {
+            var task = Task.Factory.StartNew(() => new EnviarDocumentoResponse
+            {
+                Exito = true
+            });
+            return await task;
+        }
+    }
+}
