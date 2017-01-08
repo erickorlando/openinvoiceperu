2c015e25ade39eba938b4c42c0a1b641fae1dacc
Erick Orlando <erickorlando@outlook.com>
2017-01-08 12:38:43 -0500

    #47 API REST para Resumen Diario versión 1.2 de CustomizationID

--- OpenInvoicePeru/OpenInvoicePeru.WebApi/Controllers/GenerarResumenDiarioController.cs
+++ OpenInvoicePeru/OpenInvoicePeru.WebApi/Controllers/GenerarResumenDiarioController.cs
@@ -9,23 +9,50 @@ using Microsoft.Practices.Unity;
 
 namespace OpenInvoicePeru.WebApi.Controllers
 {
+    [RoutePrefix("api/GenerarResumenDiario")]
     public class GenerarResumenDiarioController : ApiController
     {
-        private readonly IDocumentoXml _documentoXml;
+        private IDocumentoXml _documentoXml;
         private readonly ISerializador _serializador;
 
         public GenerarResumenDiarioController(ISerializador serializador)
         {
             _serializador = serializador;
-            _documentoXml = _documentoXml = UnityConfig.GetConfiguredContainer()
+            _documentoXml = UnityConfig.GetConfiguredContainer()
                 .Resolve<IDocumentoXml>(GetType().Name);
         }
 
+
+        [Route("v1")]
+        [HttpPost]
         public async Task<DocumentoResponse> Post([FromBody] ResumenDiario resumen)
         {
             var response = new DocumentoResponse();
             try
             {
+                var summary = _documentoXml.Generar(resumen);
+                response.TramaXmlSinFirma = await _serializador.GenerarXml(summary);
+                response.Exito = true;
+            }
+            catch (Exception ex)
+            {
+                response.Exito = false;
+                response.MensajeError = ex.Message;
+                response.Pila = ex.StackTrace;
+            }
+
+            return response;
+        }
+
+        [Route("v2")]
+        [HttpPost]
+        public async Task<DocumentoResponse> ResumenNuevo([FromBody] ResumenDiarioNuevo resumen)
+        {
+            var response = new DocumentoResponse();
+            try
+            {
+                // Solucion temporal --> Issue #58
+                _documentoXml = UnityConfig.GetConfiguredContainer().Resolve<IDocumentoXml>();
                 var summary = _documentoXml.Generar(resumen);
                 response.TramaXmlSinFirma = await _serializador.GenerarXml(summary);
                 response.Exito = true;
