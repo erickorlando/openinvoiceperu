74390a7220875c86398099ac35431677de062e9e
Erick Orlando <erickorlando@outlook.com>
2017-01-31 23:16:39 -0500

    #52 First attempt Guia de Remision

---
+++ OpenInvoicePeru/OpenInvoicePeru.Estructuras/DespatchAdvice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using OpenInvoicePeru.Comun;
+
+namespace OpenInvoicePeru.Estructuras
+{
+    [Serializable]
+    public class DespatchAdvice : IXmlSerializable, IEstructuraXml
+    {
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
