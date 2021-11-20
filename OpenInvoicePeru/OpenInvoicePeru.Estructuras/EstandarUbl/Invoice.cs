faa20fb9ef058c9453e009e0ac4de8e5dd2e198b
Erick Orlando <erickorlando@outlook.com>
2021-11-19 22:37:06 -0500

    Se agrega comentarios al XML de Invoice en el campo JSON Notas

--- OpenInvoicePeru/OpenInvoicePeru.Estructuras/EstandarUbl/Invoice.cs
+++ OpenInvoicePeru/OpenInvoicePeru.Estructuras/EstandarUbl/Invoice.cs
@@ -171,6 +171,8 @@ namespace OpenInvoicePeru.Estructuras.EstandarUbl
             if (DueDate != null)
                 writer.WriteElementString("cbc:DueDate", DueDate?.ToString(Formatos.FormatoFecha));
 
+
+
             writer.WriteStartElement("cbc:InvoiceTypeCode");
             {
                 writer.WriteAttributeString("listAgencyName", ValoresUbl.SchemeAgencyName);
@@ -184,12 +186,12 @@ namespace OpenInvoicePeru.Estructuras.EstandarUbl
             }
             writer.WriteEndElement();
 
-            writer.WriteStartElement("cbc:Note");
-            {
-                writer.WriteAttributeString("languageLocaleID", "1000");
-                writer.WriteValue(Note);
-            }
-            writer.WriteEndElement();
+            //writer.WriteStartElement("cbc:Note");
+            //{
+            //    writer.WriteAttributeString("languageLocaleID", "2010");
+            //    writer.WriteValue(Note);
+            //}
+            //writer.WriteEndElement();
 
             foreach (var note in NotesList)
             {
